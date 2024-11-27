1a0afd2126dcb30d7615278a5fc7346f1d390237
TheBear <drachmann.bjorn@gmail.com>
2024-11-27 22:54:30 +0100

    TCP APPs AND TRACING done!
    exquisite amount of gaming

--- p5-satellite.cc
+++ p5-satellite.cc
@@ -19,41 +19,62 @@ NS_LOG_COMPONENT_DEFINE("P5-Satellite");
 // $ cd ns-3.42
 
 
+
 // CongestionWindow Trace methods
-static void CwndTracer(uint32_t oldval, uint32_t newval) {
-    NS_LOG_UNCOND( "CWND at sim time:" << Simulator::Now().GetSeconds() << "s, CWND:" << oldval << " --> " << newval);
+static void CwndTraceSink(uint32_t nodeID, size_t socketIndex, uint32_t oldval, uint32_t newval) {
+    uint32_t roundedTime = std::round(Simulator::Now().GetSeconds()*1000)/1000;
+    NS_LOG_DEBUG("[" << roundedTime << "s] Node:" << nodeID << " Socket:" << socketIndex << " | CWND:" << oldval << " --> " << newval);
 }
-void TraceCwnd(NodeContainer n, uint32_t nodeId, uint32_t socketId) {
-    NS_LOG_UNCOND("CW Window traced Node id: " << nodeId << " socketid: " << socketId);
-    Config::ConnectWithoutContext("/NodeList/" + std::to_string(nodeId) + "/$ns3::TcpL4Protocol/SocketList/" + std::to_string(socketId) + "/CongestionWindow", MakeBoundCallback(&CwndTracer));
+// void DoTraceCwnd(uint32_t nodeID, uint32_t socketId) {
+//     NS_LOG_DEBUG("CW Window traced Node id: " << nodeID << " socketid: " << socketId);
+//     Config::ConnectWithoutContext("/NodeList/" + std::to_string(nodeID) + "/$ns3::TcpL4Protocol/SocketList/" + std::to_string(socketId) + "/CongestionWindow", MakeCallback(&CwndTraceSink)); // Or MakeBoundCallback, dont know the difference and cant see one
+// }
+
+// A supreme Trace logger, logging everything needed for a specified node!
+void DoTraceCwndSmart(Ptr<Node> node) {
+    // Get the list of sockets on the specified node
+    ObjectMapValue socketList;
+    node->GetObject<TcpL4Protocol>()->GetAttribute("SocketList", socketList);
+
+    NS_LOG_DEBUG("Node " << node->GetId() << " has " << socketList.GetN() << " sockets");
+    // For each socket on the node, enable whatever tracing is relevant!
+    for (size_t socketIndex = 0; socketIndex < socketList.GetN(); socketIndex++) {
+        Ptr<TcpSocketBase> socketBase = DynamicCast<TcpSocketBase>(socketList.Get(socketIndex));
+        
+        // Trace a source to the congestion window, but do so with a Make*Bound*Callback!
+        // This allows us to pass in additional method parameters beyond the one that CongestionWindow supports!
+        // Genius for logging!
+        socketBase->TraceConnectWithoutContext("CongestionWindow", MakeBoundCallback(&CwndTraceSink, node->GetId(), socketIndex));
+
+        // ======== ADDITIONAL SHIT IF NEEDED LATER ========
+        // Setting socket MinRTO
+        // socketBase->SetMinRto(Time(Seconds(2)));
+
+        // Setting socket Congestion control algorithm AND Recovery type?!
+        // socketBase->SetCongestionControlAlgorithm();
+        // socketBase->SetRecoveryAlgorithm();
+
+    }
 }
 
-// Custom socket functions. Delete?
-void SendData (Ptr<Socket> &socket) {
-    static const uint32_t writeSize = 1040;
-    uint8_t data[writeSize];
-    for (uint32_t i = 0; i < writeSize; ++i)
-    {
-        char m = toascii(97 + i % 26);
-        data[i] = m;
-    }
-    socket->Send(&data[0], writeSize, 0);
-}
-void ConnectSocket (Ptr<Socket> &socket, Ipv4Address destAddr, uint16_t destPort) {
-    // Connect source to sink.
-    socket->Connect(InetSocketAddress(destAddr, destPort));
+
+void ReceiveWithSeqTsSize(Ptr<const Packet> pkt, const Address& from, const Address& dst, const SeqTsSizeHeader& header) {
+    NS_LOG_DEBUG("Sink received SetTsSize pkt -> " << header);
 }
 
 
 
 
 int main(int argc, char* argv[]) {
+    // This option will make packets be randomly routed between two equally costed paths
+    // instead of always picking the same one. Is good for modelling load balancing!
     // Config::SetDefault("ns3::Ipv4GlobalRouting::RandomEcmpRouting", BooleanValue(true));
+    
     LogComponentEnable("P5-Satellite", LOG_LEVEL_ALL);
     LogComponentEnable("P5-Constellation-Handler", LOG_LEVEL_ALL);
     
     Time::SetResolution(Time::NS);
-    
+
     // ========================================= Setup default commandline parameters  =========================================
     std::string tleDataPath = "scratch/P5-Satellite/resources/starlink_13-11-2024_tle_data.txt";
     std::string tleOrbitsPath = "scratch/P5-Satellite/resources/starlink_13-11-2024_orbits.txt";
@@ -86,8 +107,10 @@ int main(int argc, char* argv[]) {
     // ================================================================
 
 
+
     // ======================== Setup constellation ========================
     Constellation LEOConstellation(satelliteCount, tleDataPath, tleOrbitsPath, groundStationsCoordinates.size(), groundStationsCoordinates, StringValue("20Mbps"), StringValue("20Mbps"));
+
     // Run simulationphase for x minutes with y second intervals. Includes an initial update at time 0.
     LEOConstellation.simulationLoop(0, 15);
 
@@ -96,23 +119,44 @@ int main(int argc, char* argv[]) {
 
 
     // ============================== APPLICATIONS ==============================
-    UdpClientHelper udp(LEOConstellation.groundStationNodes.Get(1)->GetObject<Ipv4>()->GetAddress(1, 0).GetAddress(), 7777); // Add remote addr and port
-    udp.SetAttribute("Interval", StringValue("15s"));
-    ApplicationContainer app = udp.Install(LEOConstellation.groundStationNodes.Get(0));
-    app.Start(Seconds(1));
-    app.Stop(Seconds(60*2));
-    UdpServerHelper udpS(7777);
-    app = udpS.Install(LEOConstellation.groundStationNodes.Get(1));
-    app.Stop(Seconds(60*2));
-    
+    // Inspiration from --> examples/tcp/tcp-variants-comparison.cc
+    Ptr<Node> gsNode0 = LEOConstellation.groundStationNodes.Get(0);
+    Ptr<Node> gsNode1 = LEOConstellation.groundStationNodes.Get(1);
+
+    // How to specify a congestion control, first find its TypeID
+    TypeId tcpTid = TypeId::LookupByName("ns3::TcpBbr");
+    // Version 1 - Setting for every node
+    Config::SetDefault("ns3::TcpL4Protocol::SocketType", TypeIdValue( tcpTid ));
+    // Version 2 - Setting for individual nodes
+    gsNode0->GetObject<TcpL4Protocol>()->SetAttribute("SocketType", TypeIdValue(tcpTid));
+    gsNode1->GetObject<TcpL4Protocol>()->SetAttribute("SocketType", TypeIdValue(tcpTid));
+
+
+    uint16_t port = 7777;
+    // Create a packetSink application receiving the traffic in GS 1
+    Address sinkLocalAddr(InetSocketAddress(Ipv4Address::GetAny(), port));
+    PacketSinkHelper sinkHelper("ns3::TcpSocketFactory", sinkLocalAddr);
+    sinkHelper.SetAttribute("EnableSeqTsSizeHeader", BooleanValue(true)); // Enable packet tracking, good for testing
+    // We can now enable tracing of each packet using the trace source "RxWithSeqTsSize"
+    ApplicationContainer appSink = sinkHelper.Install(gsNode1);
+    // appSink.Get(0)->TraceConnectWithoutContext("RxWithSeqTsSize", MakeCallback(&ReceiveWithSeqTsSize));
+
+    // Create a OnOff application on GS 0, streaming to GS 1
+    Ipv4Address targetIP = gsNode1->GetObject<Ipv4>()->GetAddress(1, 0).GetAddress();
+    OnOffHelper onoffHelper("ns3::TcpSocketFactory", InetSocketAddress(targetIP, port));
+    onoffHelper.SetAttribute("EnableSeqTsSizeHeader", BooleanValue(true));
+    ApplicationContainer appSource = onoffHelper.Install(gsNode0);
+    appSource.Start(Seconds(0));
+    appSource.Stop(Seconds(3));
+
+
      // ========================= TCP CWND TRACE TEST ========================
-    // Simulator::Schedule(MilliSeconds(1), &TraceCwnd, groundStations, 1 + satelliteCount, 0);
-    // *-*-*-*-* VERY IMPORTANT *-*-*-*-*
-    /* The tracer's "NodeList" is the total amount of nodes, because the satellite,
-       nodes are setup before the ground stations. Therefore to trace a specific socket,
-       we need to add the satellite count ot the index of the node.
+    // Simulator::Schedule(MilliSeconds(1), &DoTraceCwnd, gsNode0->GetId(), 0);
+    Simulator::Schedule(MilliSeconds(1), &DoTraceCwndSmart, gsNode0);
+    Simulator::Schedule(MilliSeconds(1), &DoTraceCwndSmart, gsNode1);
+    /* *-*-*-*-* VERY IMPORTANT *-*-*-*-*
+        The tracer's "NodeList" is the total amount of nodes. As the satellites are created before the ground stations, the first 2 ground stations would have ID --> satCount+1 and satCount+2 in the NodeList!
     */
-    // --------------------------------------
 
     
 
