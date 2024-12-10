92d2868b8a4e0f506a6b92f46ac4d4b8f880a189
Andersen2407 <nicolai2407@gmail.com>
2024-12-10 09:48:36 +0100

    Prettier RTT_plotter
    Enlarged the labels and axis values

--- UtilityPython/RTT_Plotter.py
+++ UtilityPython/RTT_Plotter.py
@@ -45,9 +45,12 @@ for filename in os.listdir(directory):
             plt.axvline(x=ts, color='r', label=f"linkbreak ({ts} s)")
 
         # Adding title and labels
-        plt.title(f'RTT ({filename})')
-        plt.xlabel('Time (s)')
-        plt.ylabel('Round Trip Time (ms)')
+        plt.title(f'RTT ({filename})', fontsize=36)
+        plt.xlabel('Time (s)', fontsize=30)
+        plt.ylabel('Round Trip Time (ms)', fontsize=30)
+
+        # Adjusting axis numbers font size
+        plt.tick_params(axis='both', which='major', labelsize=26)
 
         # Display the plot
         plt.grid(True)
